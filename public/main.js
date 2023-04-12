601b2826c2ab4257b21e5922d84ae3b94cfd1e6a
TimiMaliki <malikitimi@gmail.com>
2023-04-12 15:11:25 -0700

    js

--- public/main.js
+++ public/main.js
@@ -5,33 +5,104 @@ function age(){
     const myWeek = document.getElementById("day").value
 
     const activities = myWeek;
+
    
 
    switch(activities){
-      case "Monday" :
+    case "Monday":
         document.getElementById('check').innerText = "Go To Class Around 8am-2pm" + " " + " Code from 3pm-6pm "
-        + " Eat and Rest From 6:30pm-9pm" + " After That From 9:30pm-11pm , I Read My Books , Then Sleep"
+        + " Eat and Rest From 6:30pm-9pm" + " After That From 9:30pm-11pm , I Read My Books , Then Sleep";
         break;
-      case "Tuesday" :
-        document.getElementById('check').innerText = "Go To Class Around 8am-12pm" + " " + " Code from 1pm-6pm "+ " Eat and Rest From 6:30pm-9pm" + " After That From 9:30pm-11pm , I Read My Books , Then Sleep"
+      case "Monday".toUpperCase():
+        document.getElementById('check').innerText = "Go To Class Around 8am-2pm" + " " + " Code from 3pm-6pm "
+        + " Eat and Rest From 6:30pm-9pm" + " After That From 9:30pm-11pm , I Read My Books , Then Sleep";
         break;
-      case "Wednesday" :
-        document.getElementById('check').innerText =  "Go To Class Around 9am-11am" + " " + " Sports "
+      case "Monday".toLowerCase():
+        document.getElementById('check').innerText = "Go To Class Around 8am-2pm" + " " + " Code from 3pm-6pm "
+        + " Eat and Rest From 6:30pm-9pm" + " After That From 9:30pm-11pm , I Read My Books , Then Sleep";
         break;
-      case "Thursday" :
-        document.getElementById('check').innerText = "Go To Class Around 7am-8am" + " " + "  Code from 12pm-6pm" + "  Sleep"
+
+        case "Tuesday":
+          document.getElementById('check').innerText = "Go To Class Around 8am-10am" + " " + " Code from 1pm-4pm "
+          + " Eat and Rest From 6:30pm-9pm" + " After That From 9:30pm-11pm  Sleep";
+          break;        
+      case "Tuesday".toUpperCase():
+        document.getElementById('check').innerText = "Go To Class Around 8am-10am" + " " + " Code from 1pm-4pm "
+        + " Eat and Rest From 6:30pm-9pm" + " After That From 9:30pm-11pm  Sleep";
         break;
-      case "Friday" :
-        document.getElementById('check').innerText = "Thank God it's Friday" + "  Watch A Movie" + "  Sleep"
+      case "Tuesday".toLowerCase():
+        document.getElementById('check').innerText = "Go To Class Around 8am-10am" + " " + " Code from 1pm-4pm "
+        + " Eat and Rest From 6:30pm-9pm" + " After That From 9:30pm-11pm  Sleep";
         break;
-      case "Saturday" :
-        document.getElementById('check').innerText = "Cooking" + "  Watch A Movie" + "  Code" + "  Sleep"
+
+        case "Wednesday":
+          document.getElementById('check').innerText = "Sports 8am-2pm" + " " + " Code from 3pm-6pm "
+          + " Eat and Rest From 6:30pm-9pm" + " After That From 9:30pm-11pm , I Read My Books , Then Sleep";
+          break;
+      case "Wednesday".toUpperCase():
+        document.getElementById('check').innerText = "Go To Class Around 8am-2pm" + " " + " Code from 3pm-6pm "
+        + " Eat and Rest From 6:30pm-9pm" + " After That From 9:30pm-11pm , I Read My Books , Then Sleep";
         break;
-      case "Sunday" :
-        document.getElementById('check').innerText = "Reading From 10am-12pm" + "  Code from 12pm-6pm" + "  Sleep"
-        break; 
-        default : document.getElementById('check').innerText = "Enter A Valid Day"
+      case"Wednesday".toLowerCase():
+        document.getElementById('check').innerText = "Go To Class Around 8am-2pm" + " " + " Code from 3pm-6pm "
+        + " Eat and Rest From 6:30pm-9pm" + " After That From 9:30pm-11pm , I Read My Books , Then Sleep";
+        break;
+
+        case"Thursday":
+        document.getElementById('check').innerText = "Go To Class Around 7am-8am" + " " + 
+         " Church From 6:30pm-8pm" + " After That From 9:30pm-11pm , I Read My Books , Then Sleep";
+        break;
+      case"Thursday".toUpperCase():
+        document.getElementById('check').innerText = "Go To Class Around 8am-2pm" + " " + " Code from 3pm-6pm "
+        + " Eat and Rest From 6:30pm-9pm" + " After That From 9:30pm-11pm , I Read My Books , Then Sleep";
+        break;
+      case"Thursday".toLowerCase():
+        document.getElementById('check').innerText = "Go To Class Around 8am-2pm" + " " + " Code from 3pm-6pm "
+        + " Eat and Rest From 6:30pm-9pm" + " After That From 9:30pm-11pm , I Read My Books , Then Sleep";
+        break;
+
+        case"Friday":
+        document.getElementById('check').innerText = "No CLASS!!!!" + " " + " Thank God is Friday!!! "
+        + " read From 6:30pm-9pm" + "  Sleep";
+        break;
+      case"Friday".toUpperCase():
+        document.getElementById('check').innerText = "No CLASS!!!!" + " " + " Thank God is Friday!!! "
+        + " read From 6:30pm-9pm" + "  Sleep";
+        break;
+      case"Friday".toLowerCase():
+        document.getElementById('check').innerText = "No CLASS!!!!" + " " + " Thank God is Friday!!! "
+        + " read From 6:30pm-9pm" + "  Sleep";
+        break;
+
+        case"Saturday":
+        document.getElementById('check').innerText = "play basketball 6am-11am" + " " + " Code from 1pm-3pm "
+        + " Movies From 6:30pm-9pm" + " After That From 9:30pm-11pm ,Chill with my guys";
+        break;
+      case"Saturday".toUpperCase():
+        document.getElementById('check').innerText = "play basketball 6am-11am" + " " + " Code from 1pm-3pm "
+        + " Movies From 6:30pm-9pm" + " After That From 9:30pm-11pm ,Chill with my guys";
+        break;
+      case"Saturday".toLowerCase():
+        document.getElementById('check').innerText = "play basketball 6am-11am" + " " + " Code from 1pm-3pm "
+        + " Movies From 6:30pm-9pm" + " After That From 9:30pm-11pm ,Chill with my guys";
+        break;
+
+
+        case"Sunday":
+        document.getElementById('check').innerText = "Go To Church Around 8am-12pm" + "AnyThing Followsss!";
+        break;
+      case"Sunday".toUpperCase():
+        document.getElementById('check').innerText = "Go To Church Around 8am-12pm" + "AnyThing Followsss!";
+        break;
+      case"Sunday".toLowerCase():
+        document.getElementById('check').innerText = "Go To Church Around 8am-12pm" + "AnyThing Followsss!";
+        break;
+      
+
+        default : document.getElementById('check').innerText = "Enter A Valid Day , E.g Monday, MONDAY , monday"
    }
 
 }
+
+
 age();
